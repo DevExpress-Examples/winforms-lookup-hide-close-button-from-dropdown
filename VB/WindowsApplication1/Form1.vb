4c422089fc32105caf5324e36cf46c08b5d74962
DevExpressExampleBot <fakeEmail>
2023-09-15 12:20:21 +0400

    autogenerate VB

--- VB/WindowsApplication1/Form1.vb
+++ VB/WindowsApplication1/Form1.vb
@@ -1,20 +1,20 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 
-Partial Public Class Form1
-    Inherits Form
-    Public Sub New()
-        InitializeComponent()
-    End Sub
+Namespace WindowsApplication1
 
-    Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-        DataSet1.Tables(0).Rows.Add(New Object() {1, "First"})
-        DataSet1.Tables(0).Rows.Add(New Object() {2, "Second"})
-    End Sub
-End Class
+    Public Partial Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            dataSet1.Tables(CInt(0)).Rows.Add(New Object() {1, "First"})
+            dataSet1.Tables(CInt(0)).Rows.Add(New Object() {2, "Second"})
+        End Sub
+    End Class
+End Namespace
--- VB/WindowsApplication1/MyLookUpEdit.vb
+++ VB/WindowsApplication1/MyLookUpEdit.vb
@@ -1,66 +1,68 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Text
 Imports System.ComponentModel
 Imports DevExpress.XtraEditors.Registrator
 Imports DevExpress.XtraEditors
 Imports DevExpress.XtraEditors.Repository
 Imports DevExpress.XtraEditors.Popup
 
-Public Class UserLookUpEdit
-    Inherits LookUpEdit
+Namespace WindowsApplication1
 
-    Shared Sub New()
-        RepositoryItemUserLookUpEdit.Register()
-    End Sub
+    Public Class UserLookUpEdit
+        Inherits LookUpEdit
 
-    Public Sub New()
-    End Sub
+        Shared Sub New()
+            RepositoryItemUserLookUpEdit.Register()
+        End Sub
 
-    Public Overrides ReadOnly Property EditorTypeName() As String
-        Get
-            Return "UserLookUpEdit"
-        End Get
-    End Property
+        Public Sub New()
+        End Sub
 
-    <DesignerSerializationVisibility(DesignerSerializationVisibility.Content)> _
-    Public Shadows ReadOnly Property Properties() As RepositoryItemUserLookUpEdit
-        Get
-            Return TryCast(MyBase.Properties, RepositoryItemUserLookUpEdit)
-        End Get
-    End Property
-    Protected Overrides Function CreatePopupForm() As DevExpress.XtraEditors.Popup.PopupBaseForm
-        Return New UserPopupLookUpEditForm(Me)
-    End Function
-End Class
+        Public Overrides ReadOnly Property EditorTypeName As String
+            Get
+                Return "UserLookUpEdit"
+            End Get
+        End Property
 
-<UserRepositoryItem("Register")> _
-Public Class RepositoryItemUserLookUpEdit
-    Inherits RepositoryItemLookUpEdit
-    Shared Sub New()
-        Register()
-    End Sub
-    Public Sub New()
-    End Sub
+        <DesignerSerializationVisibility(DesignerSerializationVisibility.Content)>
+        Public Overloads ReadOnly Property Properties As RepositoryItemUserLookUpEdit
+            Get
+                Return TryCast(MyBase.Properties, RepositoryItemUserLookUpEdit)
+            End Get
+        End Property
 
-    Public Shared Sub Register()
-        EditorRegistrationInfo.Default.Editors.Add(New EditorClassInfo("UserLookUpEdit", GetType(UserLookUpEdit), GetType(RepositoryItemUserLookUpEdit), GetType(DevExpress.XtraEditors.ViewInfo.LookUpEditViewInfo), New DevExpress.XtraEditors.Drawing.ButtonEditPainter(), True))
-    End Sub
+        Protected Overrides Function CreatePopupForm() As PopupBaseForm
+            Return New UserPopupLookUpEditForm(Me)
+        End Function
+    End Class
 
-    <Browsable(False), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)> _
-    Public Overrides ReadOnly Property EditorTypeName() As String
-        Get
-            Return "UserLookUpEdit"
-        End Get
-    End Property
-End Class
+    <UserRepositoryItem("Register")>
+    Public Class RepositoryItemUserLookUpEdit
+        Inherits RepositoryItemLookUpEdit
 
-Public Class UserPopupLookUpEditForm
-    Inherits PopupLookUpEditForm
+        Shared Sub New()
+            Call Register()
+        End Sub
 
-    Public Sub New(ByVal owner As LookUpEdit)
-        MyBase.New(owner)
-        fCloseButtonStyle = BlobCloseButtonStyle.None
-    End Sub
-End Class
+        Public Sub New()
+        End Sub
+
+        Public Shared Sub Register()
+            Call EditorRegistrationInfo.Default.Editors.Add(New EditorClassInfo("UserLookUpEdit", GetType(UserLookUpEdit), GetType(RepositoryItemUserLookUpEdit), GetType(ViewInfo.LookUpEditViewInfo), New DevExpress.XtraEditors.Drawing.ButtonEditPainter(), True))
+        End Sub
+
+        <Browsable(False), DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)>
+        Public Overrides ReadOnly Property EditorTypeName As String
+            Get
+                Return "UserLookUpEdit"
+            End Get
+        End Property
+    End Class
+
+    Public Class UserPopupLookUpEditForm
+        Inherits PopupLookUpEditForm
+
+        Public Sub New(ByVal owner As LookUpEdit)
+            MyBase.New(owner)
+            fCloseButtonStyle = BlobCloseButtonStyle.None
+        End Sub
+    End Class
+End Namespace
